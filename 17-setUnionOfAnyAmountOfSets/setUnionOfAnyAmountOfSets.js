fbcb1144d58049c118805cd87ee1eecde05c56a3
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 16:49:37 -0500

    setUnionOfAnyAmountOfSets

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,17 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+
+  var set = new Set();
+
+  for(var i = 0; i < args.length; i++) {
+
+    args[i].forEach(set.add, set);
+  }
+
+  return set;
 }
+
+console.log(setUnionOfAnyAmountOfSets([1,2,3,4], [5,6,7,8]));
 
 // Do not edit this line;
 module.exports = setUnionOfAnyAmountOfSets;
