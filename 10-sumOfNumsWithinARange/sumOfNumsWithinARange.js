ac9c37138b6b1b4f34e0677634b417ae64372143
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 16:07:40 -0500

    sumOfNumsWithinARange

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,18 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  var total = 0;
+  for(var i = 0; i < nums.length; i++) {
+
+    if(nums[i] >= start && nums[i] <= end) {
+
+      total++;
+    }
+  }
+
+  return total;
 }
+
+console.log(sumOfNumsWithinARange( [ 10,20,30,40,50], 20, 50)) //4
 
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
