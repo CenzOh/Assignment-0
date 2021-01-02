040726bb9aa8336a809d2dd4ee9f993a1356916c
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 16:28:39 -0500

    isPalindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,20 @@
 function isPalindrome(word) {
   // Insert code here;
+
+  word = word.toLowerCase(); //so every character is the same and can be read front to back with no issues with caps
+
+  for(var i = 0; i < word.length; i++) {
+
+    if(word[i] != word[word.length - 1 - i]) { //if the first letter does not match the last letter, etc
+      return false
+    }
+  }
+
+  return true; //otherwise
+
 }
+
+console.log(isPalindrome("tacocat")); //taco cat
 
 // Do not edit this line;
 module.exports = isPalindrome;
