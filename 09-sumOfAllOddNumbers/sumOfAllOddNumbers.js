d16750874d0971632f4b40140b509be23c8f06cb
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 16:01:36 -0500

    sumOfAllOddNumbers

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,19 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+
+  var total = 0;
+
+  for(var i = 0; i < nums.length; i++) {
+
+    if(nums[i] % 2 != 0) {
+      total++;
+    }
+  }
+
+  return total;
 }
+
+console.log(sumOfAllOddNumbers([ 0,1,3,5,6,8])); //3
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
