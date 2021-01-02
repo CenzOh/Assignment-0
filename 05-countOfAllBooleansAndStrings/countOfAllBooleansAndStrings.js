e7d35b5d4aa5255261f81c6ac94a92e63aa1c3f0
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 15:18:28 -0500

    countOfAllBooleansAndStrings

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,19 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
-}
+
+  var total = 0;
+  for(var i = 0; i < arr.length; i++) {
+
+    if(arr[i] === true || arr[i] === false || typeof arr[i] === 'string') {
+
+      total++;
+    } //end of if
+  } //end of for
+
+  return total++;
+} //end of fcn
+
+console.log(countOfAllBooleansAndStrings( [true, false, 16, "sample"] )); //should output 3
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
