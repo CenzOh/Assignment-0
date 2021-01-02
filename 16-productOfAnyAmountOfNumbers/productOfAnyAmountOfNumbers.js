f690686ffdb85c4496c48ca88c334205c341a922
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 16:44:06 -0500

    productOfAnyAMountOfNumbers

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,17 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+
+  let product = 1;
+
+  for(let i of args) {
+
+    product *= i;
+  }
+
+  return product;
 }
+
+console.log(productOfAnyAmountOfNumbers(3,6,9));
 
 // Do not edit this line;
 module.exports = productOfAnyAmountOfNumbers;
