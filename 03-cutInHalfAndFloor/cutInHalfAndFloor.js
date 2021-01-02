48dcd5b738b6b1b23cab00f025dae02c2a8e8ed4
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 14:48:07 -0500

    cutInHalfAndFloor.js

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,6 +1,12 @@
 function cutInHalfAndFloor(num) {
   // Insert code here;
-}
+
+  var half = num / 2;
+  var final = Math.floor(half); //can use this math function to round down
+  return final;
+} 
+
+console.log(cutInHalfAndFloor(49));
 
 // Do not edit this line;
 module.exports = cutInHalfAndFloor;
