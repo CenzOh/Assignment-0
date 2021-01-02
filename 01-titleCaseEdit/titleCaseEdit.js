174c456b1340c24bad68d3be87a003c2307c4617
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 14:24:31 -0500

    titleCaseEdit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,18 @@
 function titleCaseEdit(title) {
   // Insert code here;
+
+  var titleArray = title.split(' '); //title in array
+
+  for(var i = 0; i < titleArray.length; i++) {
+    titleArray[i] = titleArray[i].charAt(0).toUpperCase() + //this makes the first letter of the word uppercase
+    titleArray[i].substring(1);
+
+  }
+
+  return titleArray.join(' ');
 }
+
+console.log(titleCaseEdit("green Eggs and ham")); //test
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
