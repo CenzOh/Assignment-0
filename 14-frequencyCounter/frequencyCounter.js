1afde9faf4a62035d6becf077a70776d7e7c679c
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 16:34:14 -0500

    frequencyCounter

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,23 @@
 function frequencyCounter(word) {
   // Insert code here;
+
+  var arr = [];
+
+  for(var i = 0; i < word.length; i++) {
+    var letter = word.charAt(i); //checking the letter at i
+
+    if(arr[letter]) {
+      arr[letter]++; //increment the counter for the letter by frequency
+    }
+
+    else arr[letter] = 1;
+  }
+
+  return arr;
+
 }
+
+console.log(frequencyCounter("racecar"));
 
 // Do not edit this line;
 module.exports = frequencyCounter;
