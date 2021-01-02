625d5cd27a6993db61f0bf823b1c4a4b61beb0e1
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 16:40:11 -0500

    countDownSum

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,8 +1,18 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+
+    if(num ==1) { //no need to add anything else
+      return 1;
+    }
+    
+    else if (num != 0) { 
+      return num + this.countDownSum(num - 1); //recursion
+    }
   }
 }
+
+//console.log(countDownSum(5)); //does not work in the class:(
 
 // Do not edit this line;
 let StudentSolution = MySolution;
