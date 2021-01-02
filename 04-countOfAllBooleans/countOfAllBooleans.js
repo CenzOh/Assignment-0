96d0479e35b61bb19268a73c9bc5ac49d3bac09e
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 14:57:24 -0500

    countOfAllBooleans

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,19 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
-}
+
+  var boolCount = 0;
+  for(var i = 0; i < arr.length; i++) { //check the arr for bool
+
+    if(arr[i] === true || arr[i] === false) { //did not realize i need triple =
+      boolCount++; //detects a boolean var, increase
+    } //end of if
+  } //end of for
+
+  return boolCount;
+
+} //end of function
+
+console.log(countOfAllBooleans( [1, 2, true, false] )); //should output 2
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
