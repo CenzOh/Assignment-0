cd87f3d5efbad591d067493b0d56b7cd3f8f4213
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 14:32:18 -0500

    subtractFive

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,6 +1,11 @@
 function subtractFive(num) {
   // Insert code here;
+
+  var minus = num - 5; //if num is less than five it could be a negative val?
+  return minus;
 }
+
+console.log(subtractFive(50)); //test
 
 // Do not edit this line;
 module.exports = subtractFive;
