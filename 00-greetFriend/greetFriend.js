e766ac262a7d7bd160e82c2db9357bc7021b3366
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 14:09:50 -0500

    greetFriend

--- 00-greetFriend/greetFriend.js
+++ 00-greetFriend/greetFriend.js
@@ -1,6 +1,10 @@
 function greetFriend(name) {
   // Insert code here;
+  var string = "Hello, " + name + "!";
+  return string;
 }
+
+console.log(greetFriend("Vincenzo"));
 
 // Do not edit this line;
 module.exports = greetFriend;
