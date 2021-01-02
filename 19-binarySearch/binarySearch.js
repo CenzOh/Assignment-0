c7fd658bdc6b5fbd63fde3f06636bc63ffcafd66
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 17:03:47 -0500

    binarySearch

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,8 +5,32 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+
+    var half = Math.floor(nums.length /2); //split in half to find target easier
+
+    if(nums[half] > target) {
+      var left = nums.slice(0, half); //breaking down into quarter
+
+      return this.binarySearch(left, target); //using recursion 
+    }
+
+    else if(nums[half] < target) {
+      var right = nums.slice(half + 1, nums.length); //right side
+
+      return this.binarySearch(right, target);
+    }
+
+    else if(nums[half] == target) { //found it!!
+      return true;
+    }
+
+    else { //can't find it
+      return false;
+    }
   }
 }
+
+//console.log(binarySearch( [2,4,6,8,10], 4)); //didn't work again in a class :()
 
 // Do not edit this line;
 let StudentSolution = MySolution;
