d6d04ea01d94cbbd022faa713c669a1e7c4011f1
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 15:26:00 -0500

    countOfAllNumbersSmallerThanTarget

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,19 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+
+  var total = 0;
+  for(var i = 0; i < nums.length; i++) {
+
+    if(nums[i] < target) { //will NOT count the target since not statedt "to equal to"
+
+      total++;
+    }// end of if
+  } //end of for
+
+  return total;
 }
+console.log(countOfAllNumbersSmallerThanTarget( [10,30,50,70,80,90], 80));
+//should output 4 
 
 // Do not edit this line;
 module.exports = countOfAllNumbersSmallerThanTarget;
