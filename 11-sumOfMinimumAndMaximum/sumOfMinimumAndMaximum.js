40206fc98e9d73331a8a7a60571ca80f2d684a76
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-02 16:12:15 -0500

    sumOfMinimumAndMaximum

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,15 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+
+  var min = Math.min.apply(Math, nums);
+
+  var max = Math.max.apply(Math,nums);
+
+  var sum = min + max;
+  return sum;
 }
+
+console.log(sumOfMinimumAndMaximum([1,2,3,10]));
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
